7b720f4c15687a1ad176b23b4258dc21ab9914ab
Mokhichekhra Usmonova <mokhivibes@gmail.com>
2026-05-18 06:56:59 +0500

    Created first part of the game
    Wrote the first part of the game that computer chooses and user guesses

--- guessnumbergame.py
+++ guessnumbergame.py
@@ -1,1 +1,32 @@
-print('hello world!')
+import random
+takror = True
+while takror:
+    print("Keling o'ylagan sonni topish o'ynaymiz!")
+    computer_number = random.randint(1, 10)
+    user_number = input("1 dan 10 gacha son o'yladim. Topa olasizmi?: ")
+    user_number = int(user_number)
+    count = 0
+    won = False
+    while user_number < computer_number:
+            kattaroq = input("Xato, men o'ylagan son bundan kattaroq. Yana harakat qiling: ")
+            guess = int(kattaroq)
+            count += 1
+            if computer_number == guess:
+                print(f"TOPDINGIZ! {computer_number} sonini o'ylagan edim. {count} tahminda topdingiz! Tabriklayman!")
+                won = True
+                break
+    while user_number > computer_number:
+            kichikroq = input("Xato, men o'ylagan son bunda kichikroq. Yana harakat qiling: ")
+            guess2 = int(kichikroq)
+            count += 1
+            if guess2 == computer_number:
+                print(f"TOPDINGIZ! {computer_number} sonini o'ylagan edim. {count} ta tahmin bilan topdingiz. Tabriklayman!")
+                won = True
+                break
+    if user_number == computer_number:
+            print(f"TOPDINGIZ! {computer_number} sonini o'ylagan edim. 1 ta tahmin bilan topdingiz. Tabriklayman!")
+            won = True
+    if won == True:
+        xohish = input("Yana o'ynashni xohlaysizmi? (xa uchun +, yo'q uchun -): ")
+        if xohish == "-":
+            break
